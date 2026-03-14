f707ce58f56248693178ab7c8011d53f82c08e81
avnerus <avnerus@gmail.com>
2026-03-14 00:00:59 +0000

    adding prebuilt actions

--- packages/nordic-design/src/nlp-engine.js
+++ packages/nordic-design/src/nlp-engine.js
@@ -20,6 +20,24 @@ export async function createNlp(corpus, locale = 'en') {
   const container = await dock.createContainer(uniqueName, SETTINGS, false);
   const nlp = container.get('nlp');
 
+  // Prebuild actions
+  nlp.registerActionFunction('setContext', async (data, key, value) => {
+    console.log("SET CONTEXT!", key, value);
+    data.context[key] = value;
+    return data;
+  });
+
+  nlp.registerActionFunction('increment', async (data, key) => {
+    const current = data.context[key] || 0;
+    data.context[key] = Number(current) + 1;
+    return data;
+  });
+
+  nlp.registerActionFunction('clearContext', async (data) => {
+    data.context = {};
+    return data;
+  });
+
   // Only add corpus if it has data to avoid errors
   if (corpus && corpus.data && corpus.data.length > 0) {
     await nlp.addCorpus(corpus);
